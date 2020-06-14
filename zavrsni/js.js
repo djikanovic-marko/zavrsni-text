71d3b5d8e5400d3351609f08c589386a9121d19b
djikanovic-marko <66744432+djikanovic-marko@users.noreply.github.com>
2020-06-14 13:42:08 +0200

    Add files via upload

---
+++ zavrsni/js.js
@@ -0,0 +1,12 @@
+function input() {
+    var number = document.getElementById("number").value;
+    var secondNumber = document.getElementById("second-number");
+    secondNumber.value = number;
+     }
+
+
+     function input1() {
+        var number = document.getElementById("second-number").value;
+        var secondNumber = document.getElementById("number");
+        secondNumber.value = number;
+         }
